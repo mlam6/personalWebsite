4edb8804f0f5a433c9a287243707df3227f95c42
Michelle L <michelle.lam307@gmail.com>
2020-03-09 11:26:57 -0400

    convert ES6 to ES5

--- canvas.js
+++ canvas.js
@@ -1,18 +1,18 @@
-let canvas = document.querySelector('canvas');
+var canvas = document.querySelector('canvas');
 
-let terminal = document.getElementById('terminal');
+var terminal = document.getElementById('terminal');
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerWidth / 2;
 
-let context = canvas.getContext('2d');
+var context = canvas.getContext('2d');
 
-let mouseX = canvas.width / 2;
-let mouseY = canvas.height / 2;
+var mouseX = canvas.width / 2;
+var mouseY = canvas.height / 2;
 
 document.onmousemove = handleMouseMove;
 function handleMouseMove(event) {
-    let eventDoc, doc, body;
+    var eventDoc, doc, body;
 
     event = event || window.event; // IE-ism
 
@@ -31,7 +31,7 @@ function handleMouseMove(event) {
           (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
           (doc && doc.clientTop  || body && body.clientTop  || 0 );
     }
-    let rect = canvas.getBoundingClientRect();
+    var rect = canvas.getBoundingClientRect();
     mouseX = (event.clientX - rect.left) / (rect.right - rect.left) * canvas.width;
     mouseY = (event.clientY - rect.top) / (rect.bottom - rect.top) * canvas.height;
 }
@@ -72,16 +72,16 @@ function Bit(value, color, x, y, dx, dy) {
     this.y += this.dy;
 
     // Make mouse position slightly random so that the bits don't cluster
-    let mx = mouseX + (Math.random() - 0.5) * 25;
-    let my = mouseY + (Math.random() - 0.5) * 25;
+    var mx = mouseX + (Math.random() - 0.5) * 25;
+    var my = mouseY + (Math.random() - 0.5) * 25;
 
-    let mdx = (mx - this.x);
-    let mdy = (my - this.y);
+    var mdx = (mx - this.x);
+    var mdy = (my - this.y);
 
 
-    let dist = Math.sqrt(mdx*mdx + mdy*mdy);
+    var dist = Math.sqrt(mdx*mdx + mdy*mdy);
 
-    let acc = 1 / Math.pow(dist, 1.5) * 10;
+    var acc = 1 / Math.pow(dist, 1.5) * 10;
     if (acc > 2) acc = 2;
 
     this.ddx = acc * mdx / Math.abs(mdx);
@@ -97,39 +97,39 @@ function Bit(value, color, x, y, dx, dy) {
   }
 }
 
-let bitArray = [];
+var bitArray = [];
 
 function init() {
   bitArray = [];
 
-  let bitValueArray = ['0', '1'];
-  let colorArray = ['rgb(0, 75, 98, 0.5)', 'rgb(0, 130, 143, 0.5)', 'rgb(129, 192, 197, 0.5)', 'rgb(242, 238, 237, 0.5)', 'rgb(249, 180, 1, 0.5)'];
+  var bitValueArray = ['0', '1'];
+  var colorArray = ['rgb(0, 75, 98, 0.5)', 'rgb(0, 130, 143, 0.5)', 'rgb(129, 192, 197, 0.5)', 'rgb(242, 238, 237, 0.5)', 'rgb(249, 180, 1, 0.5)'];
 
-  for (let i=0; i < 200; i++) {
-    let bitValue = bitValueArray[Math.floor(Math.random() * bitValueArray.length)];
-    let color = colorArray[Math.floor(Math.random() * colorArray.length)];
+  for (var i=0; i < 200; i++) {
+    var bitValue = bitValueArray[Math.floor(Math.random() * bitValueArray.length)];
+    var color = colorArray[Math.floor(Math.random() * colorArray.length)];
 
-    let x = Math.random() * (canvas.width - 7 * 2) + 7;
-    let y = Math.random() * (canvas.height - 7 * 2) + 7;
-    let dx = (Math.random() - 0.5) * 2;
-    let dy = (Math.random() - 0.5) * 2;
-    let ddx = 0.000000001;
-    let ddy = 0.000000001;
+    var x = Math.random() * (canvas.width - 7 * 2) + 7;
+    var y = Math.random() * (canvas.height - 7 * 2) + 7;
+    var dx = (Math.random() - 0.5) * 2;
+    var dy = (Math.random() - 0.5) * 2;
+    var ddx = 0.000000001;
+    var ddy = 0.000000001;
 
     bitArray.push(new Bit(bitValue, color, x, y, dx, dy));
   }
 }
 
 function drawTerminal() {
-  //let img = new Image();
-  let imgRatio = 516 / 319;
-  let imgWidth = canvas.width / 3;
-  let imgHeight = imgWidth / imgRatio;
-  let scalingFactor = 1.5;
-  let x = canvas.width / 4;
-  let y = -4 * (canvas.height / 5);
-  let w = imgWidth * scalingFactor;
-  let h = imgHeight * scalingFactor;
+  //var img = new Image();
+  var imgRatio = 516 / 319;
+  var imgWidth = canvas.width / 3;
+  var imgHeight = imgWidth / imgRatio;
+  var scalingFactor = 1.5;
+  var x = canvas.width / 4;
+  var y = -4 * (canvas.height / 5);
+  var w = imgWidth * scalingFactor;
+  var h = imgHeight * scalingFactor;
 
   document.getElementById('terminal').setAttribute("transform","translate(" + 0 + " " + y + ")");
 
@@ -141,7 +141,7 @@ function animate() {
   requestAnimationFrame(animate);
   context.clearRect(0, 0, innerWidth, canvas.height);
 
-  for (let i=0; i<bitArray.length; i++) {
+  for (var i=0; i<bitArray.length; i++) {
     bitArray[i].update();
   }
   drawTerminal();
